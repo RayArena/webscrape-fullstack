ed09a083e62b83c3040548ed8f3da748e2b809b7
RayArena <chandreshsingh2902@gmail.com>
2025-07-25 23:39:53 +0530

    MAke the menubar responsive and added customize not found page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
-import React, { use } from 'react';
+import { CoinsIcon, HomeIcon, Layers2Icon, MenuIcon, ShieldCheckIcon } from 'lucide-react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import Link from 'next/link';
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import { usePathname } from 'next/navigation';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { set } from 'date-fns';
 
 const routes = [
     {
@@ -17,11 +19,11 @@ const routes = [
         href: 'workflows',
         lable: 'Workflows',
         icon: Layers2Icon,
-    },{
+    }, {
         href: 'credentials',
         lable: 'Credentials',
         icon: ShieldCheckIcon,
-    },{
+    }, {
         href: 'billing',
         lable: 'Billing',
         icon: CoinsIcon,
@@ -31,7 +33,7 @@ const routes = [
 function DesktopSidebar() {
     const pathname = usePathname();
     const activeRoute = routes.find(
-        (route)=> route.href.length>0 && pathname.includes(route.href)
+        (route) => route.href.length > 0 && pathname.includes(route.href)
     ) || routes[0];
     return (
         <div className='hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate'>
@@ -40,7 +42,7 @@ function DesktopSidebar() {
             </div>
             <div className="p-2">TODO CREDITS</div>
             <div className="flex flex-col p-2">
-                {routes.map((route)=>(
+                {routes.map((route) => (
                     <Link key={route.href} href={route.href} className={buttonVariants({
                         variant: route.href === activeRoute.href ? 'sidebarActiveItem' : 'sidebarItem',
                     })}>
@@ -49,6 +51,41 @@ function DesktopSidebar() {
                     </Link>
                 ))}
             </div>
+        </div>
+    )
+}
+
+export function MobileSidebar() {
+    const [isOpen, setOpen] = useState(false);
+    const pathname = usePathname();
+    const activeRoute = routes.find(
+        (route) => route.href.length > 0 && pathname.includes(route.href)
+    ) || routes[0];
+    return (
+        <div className='block border-seperate bg-background md:hidden'>
+            <nav className='container flex items-center justify-between px-8'>
+                <Sheet open={isOpen} onOpenChange={setOpen}>
+                    <SheetTrigger asChild>
+                        <Button variant={'ghost'} size={'icon'}>
+                            <MenuIcon />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent className='w-[400px] sm:w-[540px] space-y-4' side={'left'}>
+                        <Logo />
+                        <div className="flex flex-col gap-1">
+                            {routes.map((route) => (
+                                <Link key={route.href} href={route.href} className={buttonVariants({
+                                    variant: route.href === activeRoute.href ? 'sidebarActiveItem' : 'sidebarItem',
+                                })}
+                                onClick={() => setOpen((prev) => !prev)}>
+                                    <route.icon size={20} />
+                                    {route.lable}
+                                </Link>
+                            ))}
+                        </div>
+                    </SheetContent>
+                </Sheet>
+            </nav>
         </div>
     )
 }
