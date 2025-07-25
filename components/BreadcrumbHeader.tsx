ce4f9f66321131df79d38ed370484ea91735e480
RayArena <chandreshsingh2902@gmail.com>
2025-07-25 22:58:30 +0530

    Added different themes and side menubar

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -1,11 +1,27 @@
 'use client';
 import { usePathname } from 'next/navigation';
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from './ui/breadcrumb';
 
 function BreadcrumbHeader() {
     const pathName = usePathname();
+    const paths = pathName === '/' ? [''] : pathName?.split('/');
     return (
-        <div>Breadcrumb</div>
+        <div className='flex items-center flex-start'>
+            <Breadcrumb>
+                <BreadcrumbList>
+                    {paths.map((path, index) => (
+                        <React.Fragment key={index}>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink className='capitalize' href={`/${path}`}>
+                                    {path === '' ? 'Home' : path}
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                        </React.Fragment>
+                    ))}
+                </BreadcrumbList>
+            </Breadcrumb>
+        </div>
     )
 }
 
