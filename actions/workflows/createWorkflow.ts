e19b7a74f1a70a8c47415559ccca21a78b8b4020
RayArena <chandreshsingh2902@gmail.com>
2025-08-08 20:55:40 +0530

    Done initial node part and save it part

--- actions/workflows/createWorkflow.ts
+++ actions/workflows/createWorkflow.ts
@@ -1,10 +1,15 @@
 'use server';
 
 import prisma from "@/lib/prisma";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
 import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow";
+import { AppNode } from "@/types/appNode";
+import { TaskType } from "@/types/task";
 import { WorkflowStatus } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
+import { Edge } from "@xyflow/react";
 import { redirect } from "next/navigation";
+import { json } from "zod";
 
 export async function CreateWorkflow(form: createWorkflowSchemaType) {
     const { success, data } = createWorkflowSchema.safeParse(form);
@@ -18,11 +23,18 @@ export async function CreateWorkflow(form: createWorkflowSchemaType) {
         throw new Error("unauthenticated");
     }
 
+    const initialFlow: {nodes: AppNode[], edges: Edge[]}={
+        nodes: [],
+        edges: []
+    }
+
+    initialFlow.nodes.push(CreateFlowNode(TaskType.LAUNCH_BROWSER));
+
     const result = await prisma.workflow.create({
         data:{
             userId,
             status: WorkflowStatus.DRAFT,
-            definition: "todo",
+            definition: JSON.stringify(initialFlow),
             ...data,
         }
     });
--- app/workflow/_component/FlowEditor.tsx
+++ app/workflow/_component/FlowEditor.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Workflow } from '@prisma/client';
-import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState } from '@xyflow/react';
-import React from 'react';
+import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState, useReactFlow } from '@xyflow/react';
+import React, { useEffect } from 'react';
 import '@xyflow/react/dist/style.css';
 import { CreateFlowNode } from '@/lib/workflow/createFlowNode';
 import { TaskType } from '@/types/task';
 import NodeComponent from './nodes/NodeComponent';
 
-const nodeTypes ={
+const nodeTypes = {
     ScraperFLowNode: NodeComponent,
 }
 
@@ -16,14 +16,27 @@ const snapGrid: [number, number] = [50, 50];
 const fitViewOptions = { padding: 1 }
 
 function FlowEditor({ workflow }: { workflow: Workflow }) {
-    const [nodes, setNodes, onNodesChange] = useNodesState([
-        CreateFlowNode(TaskType.LAUNCH_BROWSER),
-    ]);
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const { setViewport } = useReactFlow();
+
+    useEffect(() => {
+        try {
+            const flow = JSON.parse(workflow.definition);
+            if (!flow) return;
+            setNodes(flow.nodes || []);
+            setEdges(flow.edges || []);
+            if (!flow.viewport) return;
+            const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+            setViewport({x, y, zoom});
+        } catch (error) {
+
+        }
+    }, [workflow.definition, setNodes, setEdges, setViewport]);
     return (
         <main className="h-full w-full">
             <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} nodeTypes={nodeTypes} snapToGrid snapGrid={snapGrid} fitViewOptions={fitViewOptions} fitView>
-                <Controls position='top-left' fitViewOptions={fitViewOptions}/>
+                <Controls position='top-left' fitViewOptions={fitViewOptions} />
                 <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
             </ReactFlow>
         </main>
