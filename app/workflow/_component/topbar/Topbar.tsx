d19608c443e250d1e33ae784048621e971e4747a
RayArena <chandreshsingh2902@gmail.com>
2025-08-08 18:49:49 +0530

    Completed one node part

---
+++ app/workflow/_component/topbar/Topbar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+function Topbar() {
+    return (
+        <header className='flex p-2 border-p-2 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10'>
+            <div className="flex gap-1 flex-1"></div>
+        </header>
+    )
+}
+
+export default Topbar;
--- lib/workflow/task/registry.tsx
+++ lib/workflow/task/registry.tsx
@@ -3,3 +3,7 @@ import { LaunchBrowserTask } from "./LaunchBrowser";
 export const TaskRegistry = {
     LAUNCH_BROWSER: LaunchBrowserTask,
 }
+
+export enum TaskParamType {
+  STRING = "STRING",
+}
